6cad688fe71ab2c5644f5366d53dfbbcf223c41d
mikle8244 <mikle8244@gmail.com>
2024-01-02 19:54:28 +0900

    Jan 2

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#define _SILENCE_CXX17_C_HEADER_DEPRECATION_WARNING
+#include <bits/stdc++.h>
+using namespace std;
+#define modn 998244353
+using ll = long long int;
+using ld = long double;
+
+int main()
+{
+    return 0;
+}
